3d2f88c0d5eeb4300dda95c28d13a1199ecccc21
thedeltaproject <thedeltaproject@users.noreply.github.com>
2017-02-24 22:22:34 +0300

    Source code is now in a separate repo

--- script.py
+++ script.py
@@ -7,14 +7,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 pageList = []  # Empty list of pages
 pageListWoExt = []  # Empty list of pages without extensions
 
-for (dirpath, dirnames, filenames) in os.walk('pages'):  # Fill postList with pages' filenames
+for (dirpath, dirnames, filenames) in os.walk('../site-source-code/pages'):  # Fill postList with pages' filenames
     pageList.extend(filenames)
     break
 
 for pageName in pageList:
     pageListWoExt.append(pageName.split('.')[0])  # Fill pageListWoExt with pages' filenames w/o extensions
 
-with open('layout.html', encoding="utf-8") as layout:  # Read layout HTML, split on content and write to layoutParts
+with open('../site-source-code/layout.html', encoding="utf-8") as layout:  # Read layout HTML, split on content and write to layoutParts
     layoutParts = layout.read().split('{{ content }}\n')
 
 """
@@ -39,18 +39,18 @@ if os.path.isdir('../delta.im'):
 else:
     os.makedirs('../delta.im')  # Create new site folder
 
-with open('index.html', encoding="utf-8") as index:  # Write index.html with layout to the site folder
+with open('../site-source-code/index.html', encoding="utf-8") as index:  # Write index.html with layout to the site folder
     output = layoutParts[0] + index.read() + layoutParts[1]
     with open('../delta.im/index.html', 'w', encoding="utf-8") as outputFile:
         outputFile.write(output)
 
 for i in range(len(pageList)):  # Write pages with layout to the site folder
-    with open('pages/' + pageList[i], encoding="utf-8") as page:
+    with open('../site-source-code/pages/' + pageList[i], encoding="utf-8") as page:
         output = layoutParts[0]+page.read()+layoutParts[1]
     os.makedirs('../delta.im/' + pageListWoExt[i])
     with open('../delta.im/' + pageListWoExt[i] + '/index.html', 'w', encoding="utf-8") as outputFile:
         outputFile.write(output)
 
-copy_tree('copy', '../delta.im')  # Copy non-html files to the site folder
+copy_tree('../site-source-code/copy', '../delta.im')  # Copy non-html files to the site folder
 
 print('Success!')
