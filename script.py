fbc5124dd6ac8addfe378a411c936d3a05bc0361
thedeltaproject <thedeltaproject@users.noreply.github.com>
2017-02-27 23:31:46 +0300

    Upd

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ if os.path.isdir('../delta.im'):  # TODO: make list noDelete
     shutil.rmtree('../delta.im')  # Delete entire site folder
 """
 
-filesToDelete =[]
+filesToDelete = []
 dirsToDelete = []
 
 if os.path.isdir('../delta.im'):
@@ -52,5 +52,25 @@ for i in range(len(pageList)):  # Write pages with layout to the site folder
         outputFile.write(output)
 
 copy_tree('../site-source-code/copy', '../delta.im')  # Copy non-html files to the site folder
+
+articles = '<ol>\n'  # Make table of articles
+for i in range(len(pageList)):
+    articles += '  <li><a href="/' + pageListWoExt[i] + '">' + pageListWoExt[i] + '</a></li>\n'
+articles += '</ol>\n'
+
+for i in range(len(pageList)):  # Write table of articles
+    with open('../delta.im/' + pageListWoExt[i] + '/index.html', encoding="utf-8") as postFile:
+        post = postFile.read()
+    if post.find('{{ table }}\n') > 0:
+        post = post.replace('{{ table }}\n', articles)
+        with open('../delta.im/' + pageListWoExt[i] + '/index.html', 'w', encoding="utf-8") as outputFile:
+            outputFile.write(post)
+
+with open('../delta.im/index.html', encoding="utf-8") as postFile:
+    post = postFile.read()
+if post.find('{{ table }}\n') > 0:
+    post = post.replace('{{ table }}\n', articles)
+    with open('../delta.im/index.html', 'w', encoding="utf-8") as outputFile:
+        outputFile.write(post)
 
 print('Success!')
